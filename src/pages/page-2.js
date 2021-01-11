a66ca72ba19dadce408a03b1e542df434f5ef37f
Jimmy DC <jimmydalecleveland@gmail.com>
2021-01-11 12:22:44 -0700

    add netlify toml and print query params

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Page2 = () => {
+const Page2 = (props) => {
+  console.log(props);
   return (
     <div>
       <h1>Page 2</h1>
+      <h2>Query Params: {props.location.search}</h2>
     </div>
   );
 };
